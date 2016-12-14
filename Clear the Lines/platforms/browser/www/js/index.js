ed6dd8927042da01f2a8ca5e6662eb811e7197ce
Isaac Mercer <isaac.mercer@sparkventures.co.nz>
2016-12-14 18:54:37 +1300

    Made layout, code and structure for the app. Runs with a few sounds at the moment on iOS/Android. Looking to get full sound smorgasbord shortly.

--- Clear the Lines/platforms/browser/www/js/index.js
+++ Clear the Lines/platforms/browser/www/js/index.js
@@ -1,49 +1,38 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+function notifyonDeviceReady() {
+	document.getElementById('body').style.display = 'block';
+}
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
+function playSound(audio) {
+    console.log('loading sound' + audio);
+    if(cordova.platformId == 'android') {
+        // Play the audio file at url
+                var my_media = new Media('/android_asset/www/audio/' + audio + '.mp3',
+                    // success callback
+                    function () {
+                        console.log("playAudio():Audio Success");
+                        my_media.release();
+                    },
+                    // error callback
+                    function (err) {
+                        console.log("playAudio():Audio Error: " + err);
+                    }
+                );
+    } else {
+                var my_media = new Media('audio/' + audio + '.mp3',
+                    // success callback
+                    function () {
+                        console.log("playAudio():Audio Success");
+                        my_media.release();
+                    },
+                    // error callback
+                    function (err) {
+                        console.log("playAudio():Audio Error: " + err);
+                    }
+                );
     }
+        // Play audio
+        my_media.play();
 };
+
+document.addEventListener("deviceready", notifyonDeviceReady, false);
